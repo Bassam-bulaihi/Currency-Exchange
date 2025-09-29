025690532b9c114b8a4349be1d7ef05171809d73
Bassam-bulaihi <basssam.bulaihi.dev@gmail.com>
2025-09-29 20:12:13 +0300

    Finished step 1 and 2 :D

--- source/main.py
+++ source/main.py
@@ -1,5 +1,19 @@
+from bs4 import BeautifulSoup as soup
+import pyperclip as clip_bord
+import requests as req
 
 def main():
-    print(dir(list))
+ 
+    url = clip_bord.paste()
+
+    if url[:4] != "http" :
+        print("Invalid URL")
+        return
+    
+    response = req.get(url)
+    page_soup = soup(response.text, 'html5lib')
+    print(f"response code:{response.status_code}")
+
+    print(f"html parse: \n {page_soup.prettify()[:2000]} ...")
 
 main()         
