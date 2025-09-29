2b1d17172d1a24ace5122af9f43a82d475738057
Bassam-bulaihi <basssam.bulaihi.dev@gmail.com>
2025-09-29 21:49:52 +0300

    not much

--- source/main.py
+++ source/main.py
@@ -2,18 +2,29 @@ from bs4 import BeautifulSoup as soup
 import pyperclip as clip_bord
 import requests as req
 
+
+#aria-label= name of lising
+#price = is price
 def main():
- 
+    headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/118.0.0.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+}
     url = clip_bord.paste()
 
     if url[:4] != "http" :
         print("Invalid URL")
         return
     
-    response = req.get(url)
+    response = req.get(url,headers=headers)
     page_soup = soup(response.text, 'html5lib')
     print(f"response code:{response.status_code}")
 
-    print(f"html parse: \n {page_soup.prettify()[:2000]} ...")
+    print(f"html parse: \n {page_soup.prettify()[:300]} ...")
+
+    with open("my_page.html", "w") as f:
+        f.write(page_soup.prettify())
 
 main()         
